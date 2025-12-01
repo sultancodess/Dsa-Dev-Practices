b6935ef3d03ec1120cd573330670bcb2b0b0a0c5
sultan alam <sultancodess@gmail.com>
2025-12-01 23:16:57 +0530

    day 2

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,2 +1,3 @@
 console.log("Hello, TypeScript!");
-console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
+console.log("Hello, TypeScript!");
